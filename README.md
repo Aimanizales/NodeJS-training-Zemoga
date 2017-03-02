8fe34500822dd129d25e022de78b04e92513a927
Aiman Abdel-Samad <aiman.samad@zemoga.com>
2017-03-02 17:30:28 -0500

    Excercise 1

---
+++ README.md
@@ -0,0 +1,14 @@
+# zTraining (NodeJS)
+
+### Github repositories:
+- [Gmoralesc NodeJS](https://github.com/gmoralesc/nodejs/wiki)
+- [ZEM-NodeJS training](https://bitbucket.org/zemoga/zem-nodejs-training/wiki/Home)
+
+### Ejercicios:
+
+ - 1, **done**
+ - 2, pending
+ - 3, pending
+
+
+
--- app.js
+++ app.js
@@ -1,3 +1,12 @@
-var moment = require('moment');
-var ups = ;
-console.log(moment().format('LLLL'));
+var moment = require('moment'),
+    colors = require('colors');
+
+var hour = moment().hour();
+
+if(hour < 12){
+    console.log('Buenos días'.green);
+}else if(hour >= 12 && hour < 18){
+    console.log('Buenas tardes'.yellow);
+}else{
+    console.log('Buenas noches'.red);
+}
